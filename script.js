7eee76ae7cea320f8f511e1f2e092ba9dcb66f7a
Mike <mike@mykltd.com>
2022-04-03 23:02:00 +0100

    created HTML page.added imgs to represent rock paper or scissors. created random number generator to act as computer

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
+let randomNum= Math.floor(Math.random()*3 + 1)
+console.log(randomNum)
+ 
 
-     
-    function computerPlay(){
-        var options = ["rock", "paper", "scissors"];
+if (randomNum == 1){
+    document.getElementById("comp").src="/img/scissors.png";
+    console.log(comp)
+} else if (randomNum == 2){
+    document.getElementById("comp").src="/img/new-document.png";
+    console.log(comp)
+} else if (randomNum == 3){
+    document.getElementById("comp").src="/img/stone.png";
+    console.log(comp)
+} else {
+    var compPlay = 'OOPS'
+};
 
-        var compPlay = options[Math.floor(Math.random()*options.length)]; 
-        console.log(compPlay)
-        
-    }  
-
-     computerPlay()
-     userPlay()
-     function userPlay(){
-         var user = prompt("Choose Rock, Paper, or Scissors");
-        console.log(user)
-        return(user)
-     
-    }
-
-
-    function result(user, compPlay){
-        if (user === "rock" && compPlay === "paper"){
-            console.log("Computer picks Paper - You Lose!")
-        }
-        else if (user === "rock" && compPlay === "scissors"){
-            console.log("Computer picks Scissors - You Win!")
-        }
-        else if (user === "rock" && compPlay === "rock"){
-            console.log("Computer picks Rock - Draw!")
-        }
-        else console.log("I dont know what happened there")
-    }
-
-result()
