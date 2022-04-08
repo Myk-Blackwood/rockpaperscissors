037ea046b3c9f7206e811b63e136727a596feb1b
Mike <mike@mykltd.com>
2022-04-08 15:00:20 +0100

    need to learn to commit more often. simplified HTML and used prompt to ask for user input. looped with while loop.

--- script.js
+++ script.js
@@ -1,38 +1,81 @@
-let randomNum= Math.floor(Math.random()*3 + 1)
-console.log(randomNum);
+
+let playerScore = 0;
+let computerScore = 0;
+let games = 0;
+let playerWord = "SNAP";
+let compWord = "SNAP";
 
 
- 
+while (games < 5 && computerScore < 3 && playerScore < 3) {
+    let computerPlay= Math.floor(Math.random()*3 + 1)
+    
+    
+    function youPlay(rock, paper, scissors){
+    userSelection = window.prompt("------------------------------ Rock, Paper or Scissors? ----------------------------------- -----------------------------------Best of five games! -------------------------------------- ------------------------------ Type your selection below! --------------------------------");
 
-if (randomNum == 1){
-    document.getElementById("comp").src="/img/scissors.png";
-    console.log(comp)
-} else if (randomNum == 2){
-    document.getElementById("comp").src="/img/new-document.png";
-    console.log(comp)
-} else if (randomNum == 3){
-    document.getElementById("comp").src="/img/stone.png";
-    console.log(comp)
-} else {
-    document.getElementById("comp") 
-    document.write('OOPS')
-};
+if (userSelection.toLowerCase() == "scissors"){
+    let userPlay= 1;
+    return(userPlay);
+} else if (userSelection.toLowerCase() == "paper"){
+    let userPlay = 2;
+    return(userPlay);
+} else if (userSelection.toLowerCase() == "rock"){
+    let userPlay = 3;
+    return(userPlay);
+}
+} 
+let userPlay = youPlay();
 
-function userPlay(){
-    let myForm = document.getElementById('myForm');
-    let formData = new FormData(myForm);
-    console.log(myForm)
-if (myForm == scissors){
-    document.getElementById("user").src="/img/scissors.png";
-    console.log(comp)
-} else if (userSelect == paper){
-    document.getElementById("user").src="/img/new-document.png";
-    console.log(comp)
-} else if (userSelect == rock){
-    document.getElementById("user").src="/img/stone.png";
-    console.log(comp)
-} else {
-    var comp = 'OOPS'
-};
+function roundPlay(youPlay) {
+    if (userPlay == computerPlay){
+        alert("Draw! Draws dont count!");
+        console.log("Try again")
+        --games;
+    }else if (userPlay == 1 && computerPlay == 2){
+        alert("Computer chose Paper - You Win!");
+        document.getElementById("playerscore").innerHTML= ++playerScore;
+        playerWord = "Scissors";
+        compWord = "Paper";
+    }else if (userPlay == 1 && computerPlay == 3){
+        alert("Computer chose Rock - You Lose!");
+        document.getElementById("computerscore").innerHTML= ++computerScore;
+        playerWord = "Scissors";
+        compWord = "Rock";
+    } else if(userPlay == 2 && computerPlay == 1){
+        alert("Computer chose Scissors - You Lose!");       
+        document.getElementById("computerscore").innerHTML= ++computerScore;
+        playerWord = "Paper";
+        compWord = "Scissors";
+    } else if(userPlay == 2 && computerPlay == 3){
+        alert("Computer chose Rock - You Win!");
+        document.getElementById("playerscore").innerHTML= ++playerScore;
+        playerWord = "Paper";
+        compWord = "Rock";
+    } else if(userPlay == 3 && computerPlay == 1){
+        alert("Computer chose Scissors - You Win!");
+        document.getElementById("playerscore").innerHTML= ++playerScore;
+        playerWord = "Rock";
+        compWord = "Scissors";
+    } else if (userPlay == 3 && computerPlay == 2){
+        alert("Computer chose Paper - You Lose!");
+        document.getElementById("computerscore").innerHTML= ++computerScore;
+        playerWord = "Rock";
+        compWord = "Paper";
+    } else{
+        alert("OOPS! Thats not an option");
+        --games;
+
+    }
 }
 
+
+roundPlay();
+
+++games;
+console.log("---Round:---" ,games);
+console.log("You played:", playerWord);
+console.log("Comp played:", compWord);
+console.log("Your Score:", playerScore);
+console.log("Comps Score:", computerScore);
+
+}
